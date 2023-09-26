9d06ee9084dfac88c0c3c3df4243faa0112e40a5
Anton Prosekin <ayprosekin@gmail.com>
2023-09-26 06:16:08 +0000

    working state

--- launcher/batch.py
+++ launcher/batch.py
@@ -1,40 +1,78 @@
 from pathlib import Path
+import subprocess
 
-def script_header(slurm_directive, 
-                  shebang = "#!/bin/bash",
-                  specifier = "#SBATCH --"):
+
+def script_header(slurm_directive, shebang="#!/bin/bash", specifier="#SBATCH --"):
     header = shebang + "\n"
     for key, value in slurm_directive.items():
         if value is not None:
             header += f"\n{specifier}{key}={value}"
     header += "\n"
-    return header  
+    return header
 
 
 def script_body(app):
     cwd = app["working_dir"]
     body = f"cd {cwd}"
-    
+
     app_dir = app.get("application_dir", None)
     if app_dir is not None:
-        app_exe = str(Path(app_dir)/app["application"])
+        app_exe = str(Path(app_dir) / app["application"])
     else:
         app_exe = app["application"]
     body += f"\n{app_exe} "
-    
+
     options = app.get("options", None)
     if options is not None:
         for key, value in options.items():
             body += f"{key} {value} "
-    
+
     input_file = app.get("input_file", None)
     if input_file is not None:
         body += f"< {input_file}"
-        
-    return body    
+
+    return body
+
 
 def batch_script(job_options, app):
     script = script_header(job_options) + "\n"
     script += script_body(app)
     return script
-    
+
+
+def output_env(output_path):
+    """Prepare directories for output
+
+    Args:
+        output_path (str, Path): root path for output
+
+    Returns:
+        output_file, error_file, script_file
+    """
+    output_dir = Path(output_path)
+    script_file = output_dir / "script.sh"
+    log_dir = output_dir / "logs"
+    log_dirs = []
+    for dname in ["output", "error"]:
+        d = log_dir / dname
+        d.mkdir(parents=True, exist_ok=True)
+        log_dirs.append(d)
+
+    output_file = f"{log_dirs[0]}/%x-%A_%a.out"
+    error_file = f"{log_dirs[1]}/%x-%A_%a.err"
+    return output_file, error_file, script_file
+
+
+def run_job(slurm_directives, app_info, script_file):
+    """Run sbatch job"""
+    with open(script_file, "w") as f:
+        f.write(batch_script(slurm_directives, app_info))
+
+    proc = subprocess.run(
+        ["sbatch", f"{script_file}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    print(proc.stdout.decode())
+    err = proc.stderr.decode()
+    if err.strip():
+        print(f"stderr:\n{err}")
--- launcher/environ.py
+++ launcher/environ.py
@@ -1,47 +1,52 @@
-from batch import batch_script
-import subprocess
+from batch import output_env, run_job
 from pathlib import Path
 
 
-root_res_dir = Path("/dicos_ui_home/antonpr/work/runs/ml/erdmann_01")
-log_dir = root_res_dir / "logs"
-log_dirs = []
-for dname in ["output", "error"]:
-    d = log_dir / dname
-    d.mkdir(parents=True, exist_ok=True)
-    log_dirs.append(d)
+# root_res_dir = Path("/dicos_ui_home/antonpr/work/runs/ml/erdmann_02")
+# script_file = root_res_dir / "script.sh"
+# log_dir = root_res_dir / "logs"
+# log_dirs = []
+# for dname in ["output", "error"]:
+#     d = log_dir / dname
+#     d.mkdir(parents=True, exist_ok=True)
+#     log_dirs.append(d)
 
-output_file = f"{log_dirs[0]}/%x-%A_%a.out"
-error_file = f"{log_dirs[1]}/%x-%A_%a.err"
+# output_file = f"{log_dirs[0]}/%x-%A_%a.out"
+# error_file = f"{log_dirs[1]}/%x-%A_%a.err"
 
-slurm_directive = {
+output_file, error_file, script_file = output_env(
+    "/dicos_ui_home/antonpr/work/runs/ml/erdmann_05"
+)
+
+slurm_directives = {
     "job-name": "test",
-    "array": "0-99",
+    "array": "0-9",
     "ntasks": 1,
     "mem": "3gb",
-    "cpus-per-task": 10,
+    "cpus-per-task": 1,
     "partition": "short_serial",
     "time": "01:00:00",
     "output": output_file,
     "error": error_file,
 }
 
-app = {
+app_info = {
     "application": "python",
-    "input_file": "/dicos_ui_home/antonpr/ml/erdmann/airshower/sim_par01.py",
+    "input_file": "/dicos_ui_home/antonpr/ml/erdmann/airshower/PreProcessing_01.py",
     "working_dir": "/dicos_ui_home/antonpr/ml/erdmann/airshower",
 }
 
-# print(batch_script(job_options, app))
-script_file = Path(__file__).parent / "script.sh"
-with open(script_file, "w") as f:
-    f.write(batch_script(slurm_directive, app))
 
-proc = subprocess.run(
-    ["sbatch", f"{script_file}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-)
+run_job(slurm_directives, app_info, script_file)
 
-print(proc.stdout.decode())
-err = proc.stderr.decode()
-if err.strip():
-    print(f"stderr:\n{err}")
+# with open(script_file, "w") as f:
+#     f.write(batch_script(slurm_directive, app))
+
+# proc = subprocess.run(
+#     ["sbatch", f"{script_file}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+# )
+
+# print(proc.stdout.decode())
+# err = proc.stderr.decode()
+# if err.strip():
+#     print(f"stderr:\n{err}")
