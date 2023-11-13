c5c93a86c727d8370efdd9401e1e038c6201a53d
Anton Prosekin <ayprosekin@gmail.com>
2023-11-13 11:57:36 +0000

    gpu jobs

--- launcher/slauncher_script.py
+++ launcher/slauncher_script.py
@@ -1,24 +1,24 @@
 from slurm_launcher import output_env, run_job
 
 output_file, error_file, script_file = output_env(
-    "/dicos_ui_home/antonpr/work/runs/ml/erdmann_11"
+    "/dicos_ui_home/antonpr/work/runs/ml/gpu_03"
 )
 
 slurm_directives = {
     "job-name": "test",
-    "array": "0-9",
+    "array": "1-1",
     "ntasks": 1,
     "mem": "3gb",
+    "gpus": 1,
     "cpus-per-task": 1,
-    "partition": "short_serial",
-    "time": "01:00:00",
+    "partition": "v100_short",
+    "time": "00:02:00",
     "output": output_file,
     "error": error_file,
 }
 
 app_info = {
-    "application": "python",
-    "options": {"": "/dicos_ui_home/antonpr/ml/erdmann/airshower/PreProcessing_01.py"},
+    "application": "date\nhostname\nnvidia-smi",
     "working_dir": "/dicos_ui_home/antonpr/ml/erdmann/airshower",
 }
 
